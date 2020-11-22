b46363f5d65a44121dc6c4c83136b49b01c2d346
Vitamin-JS <Vpopchenkov@gmail.com>
2020-11-23 01:07:08 +0200

    Add apiService.js + Pixabay key

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,4 +1,9 @@
-
-
-
-// export default {функцию fetch запроса};
+export default async (name, page) => {
+    try {
+        const picturesList = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${name}&page=${page}&per_page=12&key=19224209 - dfb59b6e97d417d7c913d723b`);
+        return picturesList.json();
+    } catch (error) {
+        return error;
+    }
+}
+// было  per page =9
